83ebcd83b3d6d737da278f44e6828c26e15ccb88
wesley97 <hsiehve@gmail.com>
2022-04-07 18:06:02 +0800

    add 7-24.h

---
+++ Class/7-24.h
@@ -0,0 +1,36 @@
+#ifndef Exe_7_24
+#define Exe_7_24
+#include <iostream>
+#include <string>
+
+class Screen
+{
+public:
+    typedef std::string::size_type pos;
+    Screen() = default;
+    Screen(pos ht, pos wd) : height(ht), width(wd), contents(ht * wd, ' ') {}
+    Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c) {}
+
+    char get() const { return contents[cursor]; }
+    char get(pos ht, pos wd) const;
+    Screen &move(pos r, pos c); //函数实现代码很少，如两三行可直接在此处定义
+
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    std::string contents;
+};
+
+//显示声明为内联函数
+
+inline Screen &Screen::move(pos r, pos col)
+{
+    cursor = r * width + col;
+    return *this;
+}
+inline char Screen::get(pos r, pos col) const
+{
+    pos row = r * width;
+    return contents[row + col];
+}
+#endif
