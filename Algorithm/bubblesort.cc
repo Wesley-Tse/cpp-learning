588d07de9f4d2bcf48807c275a4f7c08a2b327c9
WSL <hsiehve@163.com>
2023-03-10 19:22:51 +0800

    fix bubblesort

--- Algorithm/bubblesort.cc
+++ Algorithm/bubblesort.cc
@@ -20,21 +20,30 @@ void swap(int arr[], int i, int j)
 
 void bubblesort(int arr[], int length)
 {
+    if (length == 1)
+        return;
+    int flag = 1;
     for (int i = length - 1; i > 0; i--)
     {
         for (int j = 0; j < i; j++)
         {
             if (arr[j] > arr[j + 1])
+            {
                 swap(arr, j, j + 1);
+                flag = 0;
+            }
         }
         cout << length - i << "th iteration: ";
         print(arr, length);
+        if (flag)
+            return;
     }
 }
 
 int main()
 {
-    int arr[] = {5, 4, 1, 0, 2, 3};
+    // int arr[] = {5, 4, 1, 0, 2, 3};
+    int arr[] = {0, 1, 2, 3, 4, 5};
     int length = sizeof(arr) / sizeof(arr[0]);
     cout << "Before Sort: ";
     print(arr, length);
