f491c117c325da1920b16f95980b03f92d1cabab
WSL <hsiehve@163.com>
2023-03-12 00:48:33 +0800

    add methods of quicksort

--- Algorithm/quicksort.cc
+++ Algorithm/quicksort.cc
@@ -3,7 +3,7 @@
 using std::cout;
 using std::endl;
 
-void print(int arr[], int length)
+void print(int *arr, int length)
 {
     for (int i = 0; i < length; i++)
     {
@@ -11,14 +11,18 @@ void print(int arr[], int length)
     }
     cout << endl;
 }
-void swap(int arr[], int i, int j)
+
+void swap(int *arr, int i, int j)
 {
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
 }
 
-int partition1(int arr[], int left, int right, int length)
+/*
+    method of two pointers
+*/
+int partition1(int *arr, int left, int right, int length)
 {
     int pivot = left;
     while (left < right)
@@ -27,14 +31,53 @@ int partition1(int arr[], int left, int right, int length)
             right--;
         while (left < right && arr[left] <= arr[pivot])
             left++;
-        swap(arr, left, right);
+        if (left < right)
+            swap(arr, left, right);
     }
-    swap(arr, pivot, right); // left = right
-    cout << "iteration: ";
+    swap(arr, pivot, right); // left == right
+    cout << "Iteration: ";
     print(arr, length);
+
     return left;
 }
-void quicksort(int arr[], int left, int right, int length)
+/*
+    method of digging
+*/
+int partition2(int *arr, int left, int right, int length)
+{
+    int pivot = arr[left];
+    while (left < right)
+    {
+        while (left < right && arr[right] >= pivot)
+            right--;
+        arr[left] = arr[right];
+        while (left < right && arr[left] <= pivot)
+            left++;
+        arr[right] = arr[left];
+    }
+    arr[left] = pivot;
+    cout << "Iteration: ";
+    print(arr, length);
+
+    return left;
+}
+
+/*
+    method of
+*/
+int partition3(int *arr, int left, int right, int length)
+{
+    int pivot = arr[left];
+    /*
+        to be added
+    */
+    cout << "Iteration: ";
+    print(arr, length);
+
+    return left;
+}
+
+void quicksort(int *arr, int left, int right, int length)
 {
     if (left >= right)
         return;
@@ -47,12 +90,14 @@ void quicksort(int arr[], int left, int right, int length)
 
 int main()
 {
-    int arr[] = {5, 3, 1, 6, 0, 2, 4, 7};
+    int arr[] = {5, 3, 1, 6, 2, 4, 9, 7, 8};
     int length = sizeof(arr) / sizeof(arr[0]);
+
     cout << "Before Sort: ";
     print(arr, length);
     quicksort(arr, 0, length - 1, length);
     cout << "After Sorted: ";
     print(arr, length);
+
     return 0;
 }
