25b235a767db2cdf1c6f0b1107223ccaeba002fa
WSL <hsiehve@163.com>
2023-03-31 00:53:55 +0800

    exercise 9-43

---
+++ Container/9-43.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void myreplace(string &s, const string oldVal, const string newVal)
+{
+    int pos = 0;
+    while (pos + oldVal.size() <= s.size())
+    {
+        if (s.substr(pos, oldVal.size()) == oldVal)
+        {
+            s.erase(pos, oldVal.size());
+            s.insert(pos, newVal);
+            pos += oldVal.size() * 2;
+            continue;
+        }
+        ++pos;
+    }
+}
+
+int main()
+{
+    string s("tho hello tho bob");
+    cout << s << endl;
+    myreplace(s, "tho", "though");
+    cout << s << endl;
+    return 0;
+}
