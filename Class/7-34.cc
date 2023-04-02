c8ba23237378efab1e451255265a033ba3853a58
WSL <hsiehve@163.com>
2023-04-02 21:32:22 +0800

    fix 7-34

--- Class/7-34.cc
+++ Class/7-34.cc
@@ -6,11 +6,11 @@ class Screen
 {
 public:
     // typedef std::string::size_type pos;
+    typedef std::string::size_type pos;
     void dummy_fcn(pos height)
     {
         cursor = width * height;
     }
-    typedef std::string::size_type pos;
 
 private:
     pos cursor = 0;
